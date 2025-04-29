ab5eb218644de3e81315479113481dc4f9f6f015
Jiwook00 <jwkim775@gmail.com>
2025-04-29 16:11:29 +0900

    first commit

---
+++ src/test/kotlin/kotlin_spring/board/BoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlin_spring.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
